7e053495951ddb170b0130aae240a8c1b56cb818
Magdalena <kokanovic422@gmail.com>
2022-10-04 16:45:21 +0200

    ...

--- FE-17-CR2-Magdalena-Kokanovic-main/js/script.js
+++ FE-17-CR2-Magdalena-Kokanovic-main/js/script.js
@@ -5,7 +5,7 @@
 let data = JSON.parse(tasks);
 
 
-document.getElementById("tasks").innerHTML += `<div class="row row-cols- row-cols-md-3 row-cols-lg-3 g-4" id="child">`;
+document.getElementById("tasks").innerHTML += `<div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4" id="child">`;
 
 
 
